026389f1a2181dcc7353b6bf2d40c6c324f95d66
AshClarke-code <64412678+AshClarke-code@users.noreply.github.com>
2021-08-26 16:27:53 +0200

    complete base date-picker layout and connect it to the input

--- src/date-picker/date-picker.component.jsx
+++ src/date-picker/date-picker.component.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
 import { ReactComponent as CalendarIcon } from "./calendar.svg";
-import { InputField, InputContainer } from "./date-picker.styles";
+import { InputField, InputContainer, CalendarContainer, CalendarHeader, CalendarHeaderContent, CalendarButton, DateContainer, DayBlock, DateBlock } from "./date-picker.styles";
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const iconStyles = {
-    width: "1.5rem",
-    height: "1.5rem"
+    width: "2.2rem",
+    height: "2.2rem"
 }
 
 const populateDates = (month, year) => {
@@ -29,24 +32,65 @@ const populateDates = (month, year) => {
 
 
     console.log(dates);
+    return dates;
 
 }
 
 const DatePicker = () => {
 
     const [iconColor, setIconColor] = useState("black");
+    const [inputValue, setInputValue] = useState("");
+    const [year, setYear] = useState(2021);
+    const [month, setMonth] = useState(1);
+    const [dates, setDates] = useState([]);
+
 
     const showCalender = event => {
         setIconColor("#00aad7");
-        populateDates(0, 2021);
+        setDates(populateDates(1, 2021));
     };
+
+    const handleClick = (date) => {
+        console.log(date.toLocaleDateString());
+        const splitDate = date.toLocaleDateString().split("/");
+
+        //DO NOT TOUCH THE TEMPLATE STRING!!!!!!!!!!!!!
+
+        const formattedDate = `${splitDate[1] < 10 ? `0${splitDate[1]}` : splitDate[1]}/${splitDate[0] < 10 ? `0${splitDate[0]}` : splitDate[0]}/${splitDate[2]}`;
+
+        console.log(formattedDate);
+
+        setInputValue(formattedDate);
+        
+    }
 
     return (
         <div>
         <InputContainer>
-        <InputField placeholder="DD/MM/YY" onFocus={showCalender}/>
+        <InputField placeholder="DD/MM/YY" onFocus={showCalender} value={inputValue}/>
         <CalendarIcon style={iconStyles} fill={iconColor}/>
         </InputContainer>
+        <CalendarContainer>
+            <CalendarHeader>
+                <CalendarButton>&#8592;</CalendarButton>
+                <CalendarHeaderContent>{`${months[month]} ${year}`}</CalendarHeaderContent>
+                <CalendarButton>&#8594;</CalendarButton>
+            </CalendarHeader>
+            <DateContainer>
+                {
+                    days.map((day, index) => {
+                        return <DayBlock key={index}>{day.slice(0, 3)}</DayBlock>
+                    })
+
+                    
+                }
+                {
+                    dates.map((date, index) => {
+                        return <DateBlock key={index + 7} value={date} onClick={() => handleClick(date)}>{date.getDate()}</DateBlock>
+                    })
+                }
+            </DateContainer>
+        </CalendarContainer>
         
         </div>
     );
--- src/date-picker/date-picker.styles.jsx
+++ src/date-picker/date-picker.styles.jsx
@@ -10,10 +10,90 @@ export const InputField = styled.input`
 `;
 
 export const InputContainer = styled.div`
-    width: 13rem;
+    width: 15rem;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: .8rem 1rem;
+    border: 1px solid grey;
+`;
+
+
+export const CalendarContainer = styled.div`
+    width: 30rem;
+    height: 30rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: .5rem;
+    box-shadow: 15px 15px 20px 1px #888888;
+`;
+
+
+export const CalendarHeader = styled.div`
+    width: 100%;
+    height: 20%;
     background-color: white;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: .4rem .6rem;
+    padding: 0 1rem;
+    box-sizing: border-box;
+`;
+
+export const CalendarHeaderContent = styled.h4`
+    text-align: center;
+    font-size: 1.6rem;
+`;
+
+export const CalendarButton = styled.button`
+    height: 60%;
+    width: 15%;
+    border: 1px solid #D7DAD9;
+    font-size: 2rem;
+    background-color: white;
+    color: grey;
+    border-radius: 5px;
+`;
+
+
+export const DateContainer = styled.div`
+    height: 80%;
+    width: 100%;
+    padding: 0 2rem;
+    padding-bottom: 2rem;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: repeat(auto, 1fr);
+`;
+
+export const DayBlock = styled.div`
+    font-size: 1.2rem;
+    color: grey:
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`;
+
+export const DateBlock = styled.div`
+    font-size: 1.2rem;
+    color: grey;
+    background-color: white;
+    border: 1px solid #D7DAD9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+        background-color: #00aad7;
+        color: white;
+        cursor: pointer;
+        transform: scale(1.1);
+        border: none;
+    }
+
 `;
